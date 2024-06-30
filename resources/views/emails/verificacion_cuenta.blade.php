eb4398b9819c26d5ccd842d149209660812e99a2
SavitarADSO <pee7491@gmail.com>
2024-06-30 17:51:40 -0500

    Blade mail

---
+++ resources/views/emails/verificacion_cuenta.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Verificación de cuenta</title>
+</head>
+<body>
+    <h1>Hola,</h1>
+    <p>Gracias por registrarte. Tu PIN de verificación es:</p>
+    <h2>{{ $pin }}</h2>
+    <p>Introduce este PIN en la aplicación para verificar tu cuenta.</p>
+</body>
+</html>
